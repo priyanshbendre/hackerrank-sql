b40d924fc5ef9d999a54fcd717ebcf2b861727ca
Priyansh Bendre <priyanshbendre@Priyanshs-MacBook-Pro.local>
2020-02-13 14:38:02 -0800

    better solution

--- basic-select/weather-observation-station-12.sql
+++ basic-select/weather-observation-station-12.sql
@@ -1,8 +1,5 @@
 -- Weather Observation Station 12
 -- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
 -- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
-
-SELECT DISTINCT City
-FROM Station
-WHERE LEFT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U') AND
-      RIGHT(City, 1) NOT IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+select distinct city from station
+where ((left(lower(city),1)) not in ('a','e','i','o','u')) AND ((right(lower(city),1)) not in ('a','e','i','o','u'))
